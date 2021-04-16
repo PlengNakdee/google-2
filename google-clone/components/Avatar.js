d1a43a1738f72e7d74b144d4a365ecf15fd07c4c
Pleng Nakdee <plengwn@gmail.com>
2021-04-17 00:42:31 +0700

    add tailwind

---
+++ google-clone/components/Avatar.js
@@ -0,0 +1,12 @@
+function Avatar({ url }) {
+  return (
+    <img
+      loading="lazy"
+      src={url}
+      alt="profile picture"
+      className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
+    />
+  );
+}
+
+export default Avatar;
--- google-clone/pages/index.js
+++ google-clone/pages/index.js
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Avatar from "../components/Avatar";
+import { MicrophoneIcon, ViewGridAddIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="flex w-full p-5 justify-between text-sm text-gray-700s">
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
         <div className="flex space-x-4 items-center">
           <p className="link">About</p>
           <p className="link">Store</p>
@@ -16,8 +20,33 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
+          <ViewGridAddIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+          <Avatar url="http://placekitten.com/300/300" />
         </div>
       </header>
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        <Image
+          src="https://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif"
+          height={140}
+          width={300}
+        />
+        <div
+          className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg 
+        max-w-md rounded-full border border-gray-200 px-5 py-3 items-center
+        sm:max-w-xl lg:max-w-2xl"
+        >
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5 text-gray-500" />
+        </div>
+        <div
+          className="flex flex-col w-1/2 space-y-2 justify-center mt-8
+        sm:space-y-0 sm:flex-row sm:space-x-4"
+        >
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Feelings Lucky</button>
+        </div>
+      </form>
     </div>
   );
 }
