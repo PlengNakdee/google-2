6733f208d19b21b1bfcd33e9e3430406363b123b
Pleng Nakdee <plengwn@gmail.com>
2021-04-21 23:49:26 +0700

    small fix

--- google-clone/components/Header.js
+++ google-clone/components/Header.js
@@ -32,6 +32,7 @@ function Header() {
             ref={searchInputRef}
             type="text"
             className="flex-grow w-full focus:outline-none"
+            defaultValue={router.query.term}
           />
           <XIcon
             className="h-7 text-gray-500 cursor-pointer transition
